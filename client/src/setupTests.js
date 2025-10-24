d5c66a34e709cf11a45b97155854d2de3f772591
arshg2343 <arsh.gupta2343@gmail.com>
2025-10-24 22:30:28 +0530

    tailwindcss fixup

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,14 +1,11 @@
-const postcss = require('postcss')
-const tailwindcss = require('tailwindcss')
-const autoprefixer = require('autoprefixer')
+const postcss = require("postcss");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  process(src) {
-    const result = postcss([
-      tailwindcss,
-      autoprefixer,
-    ]).process(src)
+	process(src) {
+		const result = postcss([tailwindcss, autoprefixer]).process(src);
 
-    return result.css
-  },
-}
+		return result.css;
+	},
+};
